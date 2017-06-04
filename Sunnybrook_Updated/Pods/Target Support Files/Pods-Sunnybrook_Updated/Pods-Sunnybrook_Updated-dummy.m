8229398533a0a28e03de361320eecb5f70d1cfab
nikpocuca <nikpocuca@Niks-MacBook-Air.local>
2017-06-04 17:08:36 -0400

    Working Build

---
+++ Sunnybrook_Updated/Pods/Target Support Files/Pods-Sunnybrook_Updated/Pods-Sunnybrook_Updated-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Sunnybrook_Updated : NSObject
+@end
+@implementation PodsDummy_Pods_Sunnybrook_Updated
+@end
---
+++ Sunnybrook_Updated/Sunnybrook_Updated/Vie_wController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sunnybrook_Updated
+//
+//  Created by Nik Pocuca on 2017-06-04.
+//  Copyright © 2017 Nik Pocuca. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
